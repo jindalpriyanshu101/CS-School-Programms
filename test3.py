6c5626a63988840033acb12e7b0725f9258d79c6
Satyaas1117 <byahutsatya191@gmail.com>
2024-05-10 14:45:00 +0530

    greatest of three numbers using conditional statement

---
+++ test3.py
@@ -0,0 +1,11 @@
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+num3 = int(input("enter third number: "))
+if num1 > num2:
+    print("num1 is the greatest")
+
+elif num2 > num3:
+    print("num2 is the greatest")   
+
+else:
+    print("num3 is the greatest")   
